feb6bdb66d46e745b46a08d6ba5bbb78d0dbb360
Thorsten Kober <info@flashdesignory.com>
2018-03-09 11:47:47 -0800

    changed webpack dev server setup

---
+++ scripts/dev.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const chalk = require('chalk');
+const open = require('open');
+const webpack = require('webpack');
+const webpackDevServer = require('webpack-dev-server');
+const config = require("../webpack.config.js");
+const compiler = webpack(config);
+
+function getExternalIp(){
+	var address,
+	    ifaces = require('os').networkInterfaces();
+	for (var dev in ifaces) {
+	    ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false ? address = details.address: undefined);
+	}
+	return address;
+}
+
+const host = getExternalIp();
+const port = 8080;
+
+const server = new webpackDevServer(compiler, {
+	contentBase: "./public",
+	historyApiFallback: true,
+	publicPath: '',
+	inline: true,
+	hot: true
+});
+
+server.listen(port, host, function(err, result){
+	if(err){
+		return console.log(err);
+	}
+
+	var url = 'http://' + host + ':' + port;
+	
+	console.log();
+	console.log(chalk.green("*******************************************"));
+	console.log();
+	console.log(chalk.cyan("listening at: " + url));
+	console.log();
+	console.log(chalk.green("*******************************************"));
+	console.log();
+
+	open(url);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,10 @@
 const path = require("path");
-const chalk = require('chalk');
-
-function getExternalIp(){
-	var address,
-	    ifaces = require('os').networkInterfaces();
-	for (var dev in ifaces) {
-	    ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false ? address = details.address: undefined);
-	}
-	return address;
-}
-
-console.log();
-console.log(chalk.green("******************************"));
-console.log();
-console.log(chalk.cyan("External IP: " + getExternalIp()));
-console.log();
-console.log(chalk.green("******************************"));
-console.log();
 
 module.exports = {
 	entry: ["./src/index.js"],
 	output: {
 		path: path.resolve(__dirname, "public"),
 		filename: "bundle.js"
-	},
-	devServer: {
-		contentBase: "./public",
-    	historyApiFallback: true,
-		port: 8080,
-		host: getExternalIp()
 	},
 	module: {
 		rules: [
@@ -67,5 +43,6 @@ module.exports = {
 	},
 	resolve: {
 	    extensions: ['.js', '.jsx', '.json']
-	}
+	},
+	mode: 'development',
 }
