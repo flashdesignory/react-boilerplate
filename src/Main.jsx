f4eab0a1aeca5220a0c53d076fe73db241925935
Thorsten Kober <info@flashdesignory.com>
2018-03-12 17:16:15 -0700

    added history event handler

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import Header from './modules/Header';
 import Footer from './modules/Footer';
 import Home from './routes/Home';
@@ -10,6 +10,22 @@ import Video from './routes/Video';
 import './Main.scss';
 
 class Main extends Component {
+	constructor(props){
+		super(props);
+
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.props.history.listen(this.handleLocationChange);
+	}
+
+	handleLocationChange(location, action) {
+		console.log(`The current URL is ${location.pathname}${location.search}${location.hash}`)
+		console.log(`The last navigation action was ${action}`)
+	}
+
+	componentDidMount(){
+		this.handleLocationChange(this.props.history.location, this.props.history.action);
+	}
+
 	render(){
 		return (
 			<div className="container">
@@ -28,4 +44,4 @@ class Main extends Component {
 	}
 }
 
-export default Main;
+export default withRouter(Main);
