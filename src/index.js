f9959a380490d2957d87a222c28d9cc146cef9d2
Thorsten Kober <info@flashdesignory.com>
2018-03-17 07:28:28 -0700

    changed to Memory Router for now

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import Main from './Main';
 import 'sanitize.css';
 import './index.scss';
 
 ReactDOM.render((
-		<BrowserRouter>
+		/* <BrowserRouter>
 			<Main/>
-		</BrowserRouter>
+		</BrowserRouter> */
+		<MemoryRouter>
+			<Main/>
+		</MemoryRouter>
 	), document.getElementById('root')
 )
