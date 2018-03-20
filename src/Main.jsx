76d85c1781679f3e5006a13859356c50343a1f1c
Thorsten Kober <info@flashdesignory.com>
2018-03-19 18:40:37 -0700

    made Main.jsx into class

--- src/Main.jsx
+++ src/Main.jsx
@@ -8,7 +8,33 @@ import Video from './routes/Video';
 
 import './Main.scss';
 
-const Main = ({ location }) => {
+class Main extends Component {
+  constructor(props){
+    super(props);
+  }
+  componentDidMount(e){
+    console.log("componentDidMount()");
+  }
+  render(){
+    const { location } = this.props;
+    return (
+  		<div className="container">
+  	      <TransitionGroup className="transition-container">
+  	        <CSSTransition key={location.key} timeout={1000} classNames="page-transition">
+  	            <Switch location={location}>
+  								<Route exact path="/" component={Home} />
+  								<Route path="/about" component={About} />
+  								<Route path="/gallery" component={Gallery} />
+  								<Route path="/video" component={Video} />
+  	            </Switch>
+  	        </CSSTransition>
+  	      </TransitionGroup>
+  		</div>
+    )
+  }
+}
+
+/*const Main = ({ location }) => {
   return (
 		<div className="container">
 	      <TransitionGroup className="transition-container">
@@ -23,6 +49,6 @@ const Main = ({ location }) => {
 	      </TransitionGroup>
 		</div>
   )
-}
+}*/
 
 export default withRouter(Main)
