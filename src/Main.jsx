21719c523c2d43bae31b23d0454615398aa18214
Thorsten Kober <info@flashdesignory.com>
2018-03-20 18:24:40 -0700

    added json preload

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import axios from 'axios';
 import Home from './routes/Home';
 import About from './routes/About';
 import Gallery from './routes/Gallery';
 import Video from './routes/Video';
+import Preloader from './modules/Preloader';
 
 import './Main.scss';
 
 class Main extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      loading: true,
+      data:undefined
+    }
   }
   componentDidMount(e){
     console.log("componentDidMount()");
+    axios.get('files/data/data.json')
+      .then(response => {
+        console.log(response.data);
+        setTimeout(() => {
+          this.setState({
+            loading: false,
+            data: response.data
+          })
+        }, 3000);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
   render(){
+    const { location } = this.props;
+    return (
+  		<div className="container">
+  	      <TransitionGroup className="transition-container">
+  	        <CSSTransition key={location.key} timeout={1000} classNames="page-transition">
+  	            <Switch location={location}>
+  								<Route exact path="/" render={() => this.state.loading ? <Preloader /> : <Home />}/>
+  								<Route path="/about" render={() => this.state.loading ? <Preloader /> : <About />}/>
+  								<Route path="/gallery" render={() => this.state.loading ? <Preloader /> : <Gallery />}/>
+  								<Route path="/video" render={() => this.state.loading ? <Preloader /> : <Video />}/>
+  	            </Switch>
+  	        </CSSTransition>
+  	      </TransitionGroup>
+  		</div>
+    )
+  }
+
+  /*render(){
     const { location } = this.props;
     return (
   		<div className="container">
@@ -31,7 +68,7 @@ class Main extends Component {
   	      </TransitionGroup>
   		</div>
     )
-  }
+  }*/
 }
 
 /*const Main = ({ location }) => {
