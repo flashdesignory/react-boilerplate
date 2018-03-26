004917ce73654dea8516a44f8b38bb86815ea36e
Thorsten Kober <info@flashdesignory.com>
2018-03-25 19:18:20 -0700

    added video button trigger

--- src/modules/video/Youtube.jsx
+++ src/modules/video/Youtube.jsx
@@ -4,15 +4,31 @@ import './Youtube.scss';
 class Youtube extends Component{
   constructor(props){
     super(props);
+    this.state={
+      visible:false
+    }
+  }
+  componentDidMount(){
+    console.log("componentDidMount()");
+    this.setState({
+      visible:true
+    })
+  }
+  componentWillUnmount(){
+    this.setState({
+      visible: false
+    })
   }
   render(){
     return (
-      <div className="video-container">
+      <div className={"video-container"}>
         <div className="youtube-container">
           <div className="youtube-player" ref="youtube-player"></div>
         </div>
-        <a className="video-container-close" href="#"><span className="icon-close"></span></a>
+        <a className="video-container-close" onClick={this.props.onClose}><span className="icon-close"></span></a>
       </div>
     )
   }
 }
+
+export default Youtube;
--- src/routes/VideoPage.jsx
+++ src/routes/VideoPage.jsx
@@ -3,8 +3,34 @@ import './VideoPage.scss';
 
 import Header from '../modules/header/Header';
 import Footer from '../modules/footer/Footer';
+import Youtube from '../modules/video/Youtube';
 
 class VideoPage extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			overlayVisible: false,
+			videoId: ""
+		}
+		this.handleOnClick = this.handleOnClick.bind(this);
+		this.handleOnClose = this.handleOnClose.bind(this);
+	}
+	handleOnClick(event){
+		console.log("handleOnClick()");
+		if(!this.state.overlayVisible){
+			this.setState({
+				overlayVisible: true
+			})
+		}
+	}
+	handleOnClose(event){
+		console.log("handleOnClose()");
+		if(this.state.overlayVisible){
+			this.setState({
+				overlayVisible: false
+			})
+		}
+	}
 	render(){
 		return (
 			<div className="page video">
@@ -12,10 +38,17 @@ class VideoPage extends Component {
 				<main>
 					<section className={"content"}>
 						<div className="content-inner">
+							<div className="video-page-trailer-button"
+								onClick={this.handleOnClick}>{this.props.landing.trailer}</div>
 						</div>
 					</section>
 				</main>
 				<Footer {...this.props.footer}/>
+				{
+					this.state.overlayVisible && (
+						<Youtube onClose={this.handleOnClose}/>
+					)
+				}
 			</div>
 		)
 	}
